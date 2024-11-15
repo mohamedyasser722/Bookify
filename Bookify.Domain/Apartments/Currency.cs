d81814c0e5f851a3c812ff4e7d898904d1ffd980
mohamedyasser722@outlook.com <mohamedyasser722@outlook.com>
2024-11-15 03:58:03 +0200

    Add project files.

---
+++ Bookify.Domain/Apartments/Currency.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookify.Domain.Apartments;
+public record Currency
+{
+    public string Code { get; init; }
+    private Currency(string code) => Code = code;
+
+
+
+    internal static readonly Currency None = new("");
+    public static readonly Currency USD = new Currency("USD");
+    public static readonly Currency EUR = new Currency("EUR");
+    public static readonly IReadOnlyCollection<Currency> All = new Currency[] { USD, EUR };
+    public static Currency FromCode(string code)
+    {
+        return All.FirstOrDefault(c => c.Code == code) ??
+             throw new ApplicationException($"Currency with code {code} is invalid");
+    }
+}
---
+++ Bookify.Domain/Apartments/Money.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookify.Domain.Apartments;
+public record Money(decimal Amount, Currency Currency)
+{
+    public static Money operator +(Money left, Money right)
+    {
+        if (left.Currency != right.Currency)
+        {
+            throw new InvalidOperationException("Cannot add money with different currencies");
+        }
+
+        return new Money(left.Amount + right.Amount, left.Currency);
+    }
+
+    public static Money operator -(Money left, Money right)
+    {
+        if (left.Currency != right.Currency)
+        {
+            throw new InvalidOperationException("Cannot subtract money with different currencies");
+        }
+
+        return new Money(left.Amount - right.Amount, left.Currency);
+    }
+
+   public static Money Zero() => new Money(0, Currency.None);
+
+    public override string ToString()
+    {
+        return $"{Amount} {Currency}";
+    }
+}
