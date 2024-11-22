72722e46b7007fea937a0e57480c39d466e22d7e
mohamedyasser722@outlook.com <mohamedyasser722@outlook.com>
2024-11-22 04:26:49 +0200

    Done With Application Layer

--- Bookify.Domain/Abstractions/Error.cs
+++ Bookify.Domain/Abstractions/Error.cs
@@ -2,5 +2,5 @@
 public record Error(string code, string name)
 {
     public static Error None = new(string.Empty, string.Empty);
-    public static Error NullValue = new("Error.NullValue", "Null value was provided";
+    public static Error NullValue = new("Error.NullValue", "Null value was provided");
 }
--- Bookify.Domain/Abstractions/Result.cs
+++ Bookify.Domain/Abstractions/Result.cs
@@ -5,9 +5,14 @@ public class Result
 {
     protected internal Result(bool isSuccess, Error error)
     {
+        if (isSuccess && error != Error.None)
+        {
+            throw new InvalidOperationException();
+        }
+
         if (!isSuccess && error == Error.None)
         {
-            throw new InvalidOperationException("An unsuccessful result must contain an error message.");
+            throw new InvalidOperationException();
         }
 
         IsSuccess = isSuccess;
@@ -15,27 +20,37 @@ public class Result
     }
 
     public bool IsSuccess { get; }
+
     public bool IsFailure => !IsSuccess;
+
     public Error Error { get; }
-    public static Result Success() => new Result(true, Error.None);
-    public static Result Failure(Error error) => new Result(false, error);
-    public static Result<TValue> Success<TValue>(TValue value) => new Result<TValue>(value, true, Error.None);
-    public static Result<TValue> Failure<TValue>(Error error) => new Result<TValue>(default, false, error);
-    public static Result<TValue> Create<TValue>(TValue? value)
-        => value is not null ? Success(value) : Failure<TValue>(Error.None);
 
+    public static Result Success() => new(true, Error.None);
 
+    public static Result Failure(Error error) => new(false, error);
+
+    public static Result<TValue> Success<TValue>(TValue value) => new(value, true, Error.None);
+
+    public static Result<TValue> Failure<TValue>(Error error) => new(default, false, error);
+
+    public static Result<TValue> Create<TValue>(TValue? value) =>
+        value is not null ? Success(value) : Failure<TValue>(Error.NullValue);
 }
+
 public class Result<TValue> : Result
 {
-    private readonly TValue _value;
+    private readonly TValue? _value;
+
     protected internal Result(TValue? value, bool isSuccess, Error error)
         : base(isSuccess, error)
     {
         _value = value;
     }
+
     [NotNull]
-    public TValue Value => IsSuccess ? _value : throw new InvalidOperationException("Cannot access value for a failed result.");
+    public TValue Value => IsSuccess
+        ? _value!
+        : throw new InvalidOperationException("The value of a failure result can not be accessed.");
 
     public static implicit operator Result<TValue>(TValue? value) => Create(value);
 }
